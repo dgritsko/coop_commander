51564ef115d4068b582fb88411794ac493481a72
Dan Gritsko <dgritsko@gmail.com>
2017-12-08 02:11:23 -0500

    Adding some functions to powerup

--- js/game.powerup.js
+++ js/game.powerup.js
@@ -17,6 +17,9 @@ class Powerup {
     constructor(game) {
         this.id = Math.floor(Math.random() * PowerupTypes.length);
 
+        this.spawnTime = game.time.now;
+        this.lifetime = 1000 * 5;
+
         // TODO: Restrict the area in which powerups can spawn a little more than just "anywhere"
 
         var x = Math.random() * game.world.width;
@@ -27,6 +30,18 @@ class Powerup {
     }
 }
 
+Powerup.prototype.update = function(game) {
+    if (game.time.now > (this.spawnTime + this.lifetime)) {
+        this.kill();
+    }
+}
+
+Powerup.prototype.kill = function() {
+    // TODO
+}
+
 Powerup.prototype.pickup = function() {
     // TODO: Affect the game state based on the powerup's type
+
+    this.kill();
 }
