4c5466fd79f2321d4c0bb05c383f5ab21dd0bc02
Dan Gritsko <dgritsko@gmail.com>
2017-12-10 22:03:22 -0500

    Implement "scores.php"

---
+++ api/scores.php
@@ -0,0 +1,64 @@
+<?php
+include('api_key.php');
+
+// http://docs.mlab.com/data-api/
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+$url = 'https://api.mlab.com/api/1/databases/coopdefender/collections/scores?apiKey=' . $API_KEY;
+
+if ($method == 'GET') {
+	$url = $url . '&f={"_id":0,"date":0}&s={"score":-1}&l=10';
+		
+	// Initiate curl
+	$ch = curl_init();
+	// Disable SSL verification
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	// Will return the response, if false it print the response
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_URL,$url);
+	$result=curl_exec($ch);
+	curl_close($ch);
+
+	header('Content-Type: application/json');
+
+	echo $result;
+} else if ($method == 'POST') {
+	$score = $_GET['score'];
+	$name = $_GET['name'];
+	$level = $_GET['level'];
+		
+	if (empty($score) || empty($name) || empty($level)) {
+		http_response_code(404);
+		die();
+	} else {
+		$date = substr(date('c', time()), 0, 19) . 'Z';
+		
+		$request = array(
+			'score' => (int)$score, 
+			'name' => $name,
+			'level' => (int)$level,
+			'date' => array('$date' => $date)
+			);
+		
+		$json = json_encode($request);
+		
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $json);  
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
+			'Content-Type: application/json',                                                                                
+			'Content-Length: ' . strlen($json))                                                                       
+		); 
+		
+		curl_setopt($ch, CURLOPT_URL,$url);
+		$result=curl_exec($ch);
+		curl_close($ch);
+	}
+} else {
+	http_response_code(404);
+	die();
+}
+
+?>
